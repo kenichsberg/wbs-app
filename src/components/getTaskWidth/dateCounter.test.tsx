b4f3f4e1e7911ebb7e3490680cd8c9a85d7b3ac7
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-07-14 11:19:29 +0900

    fix to add time for parse str of Moment

--- src/components/getTaskWidth/dateCounter.test.tsx
+++ src/components/getTaskWidth/dateCounter.test.tsx
@@ -1,12 +1,14 @@
 import { getLeftEndDate, getRightEndDate, getWeekCount } from './dateCounter';
 import * as consts from '/components/GanttChart/consts';
+import { Moment } from 'moment';
 
 const moment = require('moment');
 
-const dates: Array<Date> = [
-  moment('2020/7/7'),
-  moment('2020/7/9'),
-  moment('2020/7/20')
+
+const dates: Array<Moment> = [
+  moment('2020-07-07 21:00:00', 'YYYY-MM-DD hh:mm:ss'),
+  moment('2020-07-09 14:00:00', 'YYYY-MM-DD hh:mm:ss'),
+  moment('2020-07-20 11:00:00', 'YYYY-MM-DD hh:mm:ss'),
 ];
 
 const leftSunday = getLeftEndDate(dates);
@@ -15,15 +17,17 @@ const rightSaturday = getRightEndDate(dates);
 describe('dateCounter', () => {
   describe('getMostLeftDate method', () => {
     it('should calculate left date', () => {
+      const expected = moment('2020-7-5 00:00:00', 'YYYY-MM-DD hh:mm:ss');
 
-      expect(leftSunday).toEqual(moment('2020/7/5').toDate());
+      expect(leftSunday.format('LL')).toBe(expected.format('LL'));
     });
   });
 
   describe('getMostRightDate method', () => {
     it('should calculate right date', () => {
+      const expected = moment('2020-7-25 23:59:59', 'YYYY-MM-DD hh:mm:ss');
 
-      expect(rightSaturday).toEqual(moment('2020/7/25').toDate());
+      expect(rightSaturday.format('LL')).toBe(expected.format('LL'));
     });
   });
 
