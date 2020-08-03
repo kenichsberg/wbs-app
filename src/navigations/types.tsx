c10506e4121f87de78415a28678bd06cd81286e7
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-08-03 19:12:02 +0900

    add spaces

--- src/navigations/types.tsx
+++ src/navigations/types.tsx
@@ -2,9 +2,9 @@ import { StackScreenProps } from '@react-navigation/stack';
 import { Task } from '/screens/CreateTaskScreen';
 
 export type ScheduleStackParamList = {
-  TaskList: {task:Task} | undefined;
-  CreateTask: {task: Task} | undefined;
-  EditTask: {task: Task} | undefined;
+  TaskList: { task:Task } | undefined;
+  CreateTask: { task: Task } | undefined;
+  EditTask: { task: Task } | undefined;
 };
 
 export type TaskListProps = StackScreenProps<ScheduleStackParamList, 'TaskList'>;
