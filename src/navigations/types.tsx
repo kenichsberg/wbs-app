41241393bbea992ff7d9c2399aecc76377200a51
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-08-04 23:27:40 +0900

    add types

--- src/navigations/types.tsx
+++ src/navigations/types.tsx
@@ -1,17 +1,34 @@
 import { StackScreenProps } from '@react-navigation/stack';
+import { MaterialTopTabScreenProps } from '@react-navigation/material-top-tabs';
 import { Task } from '/screens/CreateTaskScreen';
 
+// type of navigator
 export type ScheduleStackParamList = {
-  TaskList: { task:Task } | undefined;
+  TaskList: { task: Task } | undefined;
   CreateTask: { task: Task } | undefined;
   EditTask: { task: Task } | undefined;
 };
 
+// type of screen
 export type TaskListProps = StackScreenProps<ScheduleStackParamList, 'TaskList'>;
 export type CreateTaskProps = StackScreenProps<ScheduleStackParamList, 'CreateTask'>;
 
+
+// type of navigator
+export type ScheduleTabParamList = {
+  LIST: { task: Task } | undefined;
+  CALENDAR: { task: Task } | undefined;
+};
+
+// type of screen
+export type ListTabProps = StackScreenProps<ScheduleTabParamList, 'LIST'>;
+export type CalendarTabProps = StackScreenProps<ScheduleTabParamList, 'CALENDAR'>;
+
+
+// type of navigator
 export type SettingsStackParamList = {
   Settings: undefined;
 };
 
+// type of screen
 export type SettingsProps = StackScreenProps<SettingsStackParamList, 'Settings'>;
