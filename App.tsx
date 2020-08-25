6a7e6bd2b12451587a4c27039fb5c184e2c00498
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-08-25 16:05:35 +0900

    move to src/index.tsx

--- App.tsx
+++ App.tsx
@@ -1,37 +1,7 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { AppNavigator } from './src/navigations/AppNavigator';
+import { Root } from './src/';
 
 
-const App: React.FC = () => { 
-  const [isReady, setIsReady] = React.useState<boolean>(false);
-
-  async function init() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    setIsReady(true);
-  }
-
-  React.useEffect(() => {
-    init();
-  }, []);
-
-  if (!isReady) {
-    return <View></View>;
-  }
-
-  return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
-  );
-}
+const App: React.FC = () =>  <Root />; 
 
 export default App;
