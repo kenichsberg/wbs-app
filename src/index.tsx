7f422d64817b2bb3101b73e3bf08cc93a6a87b70
東洋システム <nakayama_kenichi@toyosystem.co.jp>
2020-09-22 18:44:30 +0900

    feat: add to handle login

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
-//import { View } from 'react-native';
 import { AppLoading } from 'expo';
 import 'react-native-gesture-handler';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import { User } from '@firebase/auth-types/';
 import { AppNavigator } from '/navigations/AppNavigator';
 import { AppStateProvider } from '/contexts/AppStateContext';
+import { LoginScreen } from '/screens/LoginScreen/';
+import { firebase } from '/data-access/firebase';
 
 import '/data-access/fixtimerbug';
 
 
 export const Root: React.FC = () => { 
   const [isReady, setIsReady] = React.useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = React.useState<boolean>(false);
 
-  async function init() {
+  const init = async() => {
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
@@ -24,10 +27,23 @@ export const Root: React.FC = () => {
 
   React.useEffect(() => {
     init();
+
+    firebase.auth().onAuthStateChanged((user: User | null) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    });
+
   }, []);
 
   if (!isReady) {
     return <AppLoading />;
+  }
+
+  if (!isLoggedIn) {
+    return <LoginScreen />
   }
 
   return (
